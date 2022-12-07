d0a5883f3fcb36a162684626a012847409eaf343
Mohammad Azimi <mohammadazimi91@gmail.com>
2022-12-07 22:51:47 +0330

    added react-native-bottom-sheet

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={styles.rootGesture}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
+  rootGesture:{
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
